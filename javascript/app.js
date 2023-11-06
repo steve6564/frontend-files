19d0a995aa64a2f87ffe8f81f2cae800f2241242
steve6564 <bukolaoluwasegunstephen@gmail.com>
2023-11-06 10:36:49 -0800

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,114 @@
+// console.log("Good afternoon");
+// /* dttr
+// // TextDecoder
+//  ftfytyt */
+// var age = 17;
+// console.log(age);
+// age = 23.45;
+// //console.log(age)
+
+// let names = "Dapo";
+// //console.log(names);
+
+// const school = "KWASU";
+// //school = "Unilorin";
+// //console.log(school);
+
+// console.log(typeof(age));
+// var c = 'errtyy'
+// console.log(c + "dada");
+
+// var fruits = ["Mango", "Cashew", "Guava", [23, 34], "Berry"];
+// console.log(fruits[3][0]);
+
+// var contact = {"kunle":70345678, 'Flora':8098765432}
+// console.log(contact["kunle"]);
+// console.log(contact.Flora);
+
+// var num = 23;
+// //console.log(num.toExponential(2))
+// num = num.toString()
+// console.log(typeof(num))
+
+// var state = "     Kwara is a state     ";
+// console.log(state.charAt(3))
+// // console.log(state[3])
+// console.log(state.includes('ra'))
+// const f = console.log;
+// f(state.replace('is', "as"))
+// f(state.trim())
+
+
+// // Array Methods
+// var car = ["Toyota", "Benz", "Rolls Royce"];
+// car.shift()
+// car.unshift("Chevrolet")
+// car.pop()
+// car.push()
+// f(car)
+
+
+// var a = 17;
+// var d = 34;
+// var b = "17";
+var c = console.log;
+// // console.log(a == b);
+// // console.log(a === b);
+// c(++a);
+// c(--d)
+// c(d**2)
+
+// // logical operators
+// c(a < d && d > a)
+// c(a > d || d > a)
+// c(a != d)
+
+var time = 11;
+// if (time == 12) {
+//     c("Good Morning");
+// } else if (time == 11) {
+//     c("Good Evening");
+// }
+// else {
+//     c("Good Day")
+// }
+
+// switch (time) {
+//     case 10:
+//         c("Wrong");
+//         break;
+//     case 12:
+//         c("Right");
+//         break;
+//     default:
+//         c("Sorry couldnt find the right value")
+//         break;
+// }
+
+
+
+// var town = "tanke";
+// switch (town) {
+//     case 'ilorin':
+//         c("Goood town");
+//         break;
+//     case 'tanke':
+//         c("Right place");
+//         break;
+//     default:
+//         c("Sorry couldnt find the right value")
+//         break;
+// }
+
+//FOR LOOP
+// for (var i = 0; i < 6; ++i) {
+//     c(i);
+// 
+
+for (var i = 10; i >= 0; --i) {
+    if (i == 0) {
+        c("HAPPY NEW YEAR");
+        break;
+    }
+    c(i);
+}
